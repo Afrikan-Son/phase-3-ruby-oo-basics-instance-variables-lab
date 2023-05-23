6cad211b1712a4b387de43baf35d86ffd99e4f3d
Mark Wanjau <theafrikanson@gmail.com>
2023-05-24 00:10:47 +0300

    completed assignment with bosco

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+    def name=(dog_name)
+      @this_dogs_name = dog_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
 
 end
+  
+  bosco = Dog.new
+  
+  bosco.name = "Bosco"
+  
+  puts bosco.name
